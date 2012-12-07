ec3ae72ba7eef86e058148e85d3a8ec619ccc29a
ankitgoel1987 <ankitgoel1987@gmail.com>
2012-12-07 13:06:29 +0000

    Added rightscale_marker to default.rb

--- cookbooks/intermediate1/metadata.rb
+++ cookbooks/intermediate1/metadata.rb
@@ -5,4 +5,6 @@ description      "Installs/Configures intermediate1"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+depends "rightscale"
+
 recipe "intermediate1::default","Prints hello world output"  
